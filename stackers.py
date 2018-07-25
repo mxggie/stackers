6e4eab59511a3b5ad105e729d95b026bddf58be3
mxggie <maggieschwierking@gmail.com>
2018-07-25 15:50:02 -0500

    game complete

--- stackers.py
+++ stackers.py
@@ -5,6 +5,7 @@ import time
 #this script demostrates how to create a class structure for gaming mode
 sense = SenseHat()
 sense.clear()
+turqouise = (0, 255, 200)
 
 class stack():
     def __init__(self):
@@ -16,13 +17,28 @@ class stack():
         pygame.time.set_timer(USEREVENT +1, 700)
         n = 0
         r = 7
+        x = 0
+        y = 0
+        z = 0
         while self.gaming:
             for event in pygame.event.get():        
-                if event.type == KEYDOWN:   
-                    if r < 1:
-                        print "Game Over"                        
-                        self.gaming = False   
-                    sense.set_pixel(n, r, (0,255,0))                               
+                if event.type == KEYDOWN:                   
+                    if r == 7:
+                        x = n 
+                    if r == 5:
+                        y = n
+                    if r == 3:
+                        z = n               
+                    if r <= 1:
+                        if x == n and y == n and z == n:
+                            sense.show_message("You Won!", 0.05, text_colour=turqouise)                      
+                            self.gaming = False  
+                            exit()
+                        else:
+                            sense.show_message("You Lost", 0.05, text_colour=turqouise)                     
+                            self.gaming = False
+                            exit()
+                    sense.set_pixel(n, r, (0,255,0))                    
                     r -= 1
                     
                 else:                    
